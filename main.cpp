2a300ea7bf6fc1a60fdd04368aea8ffffc57c4e9
David Soane <david.soane@outlook.com>
2024-08-01 20:18:32 -0600

    accepting user input

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include <vector>
 
 int main() {
-    
-}
+
+    std::vector<std::string> args_vector;
+    std::string args;
+
+        std::cout << "Custom Linux Shell" << std::endl;
+
+        std::cout << " > ";  
+        std::cin >> args;
+        std::cout << typeid(args).name();
+       
+};
