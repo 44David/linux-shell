9babc4bf7effae1108597b11234d685636e45f8d
David Soane <david.soane@outlook.com>
2024-08-03 16:38:09 -0600

    added reading files command

--- main.cpp
+++ main.cpp
@@ -3,28 +3,7 @@
 #include <regex>
 #include <filesystem>
 #include <string>
-
-int main() {
-
-    std::vector<std::string> args_vector;
-    std::string args;
-
-    std::cout << "Custom Linux Shell" << std::endl;
-
-    std::cout << " > ";  
-    std::cin >> args;
-        
-    // TODO: fix this regular expression
-    std::regex e ("([a-zA-Z])\w+)");
-    if ( std::regex_match(e, args) ) {
-        std::cout << "Output is a string";
-    } else {
-        "Unknown output"
-    }
-
-    return 0;
-       
-};
+#include <fstream>
 
 int find_files() {
     // TODO: make output more readable
@@ -34,5 +13,52 @@ int find_files() {
         std::cout << entry.path() << std::endl;
 
     return 0;
+};
 
-}
+int read_file(std::string filename) {
+    std::ifstream readfile(filename);
+    
+    std::string line;
+
+    while (getline ( readfile, line )) {
+        std::cout << line << std::endl;
+    };
+    
+    return 0;
+
+};
+
+
+
+int main() {
+
+    std::vector<std::string> args_vector;
+    std::string arg1, arg2;
+
+    std::cout << "Custom Linux Shell" << std::endl;
+
+    std::cout << " > ";  
+
+    std::cin >> arg1;
+
+    if (arg1 == "ls") {
+        find_files();
+    }; if (arg1 == "cat") {
+        read_file("hello.txt");
+    };
+
+
+
+        
+    // TODO: fix this regular expression
+    // std::regex e ("([a-zA-Z])\w+)");
+    // if ( std::regex_match(e, args) ) {
+    //     std::cout << "Output is a string";
+    // } else {
+    //     "Unknown output"
+    // }
+
+    return 0;
+       
+};
+
